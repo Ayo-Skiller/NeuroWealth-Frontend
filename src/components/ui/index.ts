6b14276ba3a849e26a3ffb8129c4304da0375d89
edehvictor <edehvictor715@gmail.com>
2026-03-28 19:35:51 +0100

    feat: add reusable form validation system (#87)

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -6,6 +6,7 @@ export * from "./Button";
 export * from "./Card";
 export * from "./EmptyState";
 export * from "./ErrorPage";
+export * from "./FormErrors";
 export * from "./Input";
 export * from "./Modal";
 export type { ModalSize } from "./Modal";
